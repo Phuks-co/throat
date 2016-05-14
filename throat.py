8e094abe1255580c65c17019f476ec89645c7b3a
Ramiro Bou <bouramiro@gmail.com>
2016-05-14 16:35:35 -0300

    Initial commit.

---
+++ throat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
