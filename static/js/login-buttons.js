081b0eb1b61e55af8c592d5e0d83cb26bdef951f
Miguel Peláez <miguel2706@outlook.com>
2016-05-16 14:54:00 -0500

    Add pop-up (popover)

---
+++ static/js/login-buttons.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	$('a.btn.login').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#nick',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+});
+$(document).ready(function() {
+	$('a.btn.register').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#nick',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+});
