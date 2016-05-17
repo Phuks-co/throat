7697e3a080c973fc8c3f55ce3b70264023887ed4
Ramiro Bou <bouramiro@gmail.com>
2016-05-17 01:22:10 -0300

    Minor changes to save some bandwidth

--- static/js/site.js
+++ static/js/site.js
@@ -13,4 +13,24 @@ $(document).ready(function () {
         e.preventDefault();
 
     });
+    
+    var mpSettings = {
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#username';
+				}
+			}
+		}
+	};
+    $('a.btn.register').magnificPopup(mpSettings);
+    $('a.btn.login').magnificPopup(mpSettings);
 });
