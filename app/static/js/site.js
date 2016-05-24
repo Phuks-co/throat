98d81d6b3e50b86978d03a5b040eb3d426cedec7
pembo210 <pembo210@users.noreply.github.com>
2016-05-23 20:16:37 -0400

    more tmp nightmode

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -179,7 +179,8 @@ function checkMode() {
     //create <style> in head
     var style = document.createElement('style');
     style.type = 'text/css';
-    style.innerHTML = 'body{background:#101010}';
+    style.innerHTML = 'body,header,footer,nav ul li a,.center,.post.title,.side';
+    style.innerHTML += '{background: #101010;color: #fff;}';
     document.getElementsByTagName('head')[0].appendChild(style);    
     }
 }
