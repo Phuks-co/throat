33287d2cdec5290f8cd0f72a92dde91e8343d2d2
Ramiro Bou <bouramiro@gmail.com>
2016-05-25 00:23:44 -0300

    Making the email field truly optional.

--- app/forms/user.py
+++ app/forms/user.py
@@ -2,6 +2,7 @@
 from flask_wtf import Form, RecaptchaField
 from wtforms import StringField, PasswordField, TextField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, Required, EqualTo
+from wtforms.validators import Optional
 from wtforms.fields.html5 import EmailField
 
 
@@ -13,11 +14,26 @@ class LoginForm(Form):
                                                      Length(min=7, max=256)])
 
 
+class OptionalIfFieldIsEmpty(Optional):
+    def __init__(self, field_name, *args, **kwargs):
+        self.field_name = field_name
+        super(OptionalIfFieldIsEmpty, self).__init__(*args, **kwargs)
+
+    def __call__(self, form, field):
+        other_field = form._fields.get(self.field_name)
+        if other_field is None:
+            raise Exception('no field named "{0}" in form'
+                            .format(self.field_name))
+        if other_field.data == '':
+            super(OptionalIfFieldIsEmpty, self).__call__(form, field)
+
+
 class RegistrationForm(Form):
     """ Registration form. """
     username = TextField('Username', [Length(min=2, max=32)])
-    email = EmailField('Email Address',
-                       validators=[Email("Please enter your email address.")])
+    email = EmailField('Email Address (optional)',
+                       validators=[OptionalIfFieldIsEmpty('email'),
+                                   Email("Invalid email address.")])
     password = PasswordField('Password', [
         Required(),
         EqualTo('confirm', message='Passwords must match'),
