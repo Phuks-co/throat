17af23654a930ffa05ad5584b33a4373825b110e
Ramiro Bou <bouramiro@gmail.com>
2016-05-27 01:15:01 -0300

    Updating wsgi.py for python 3

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+""" This is for openshift to run this app. """
+
 import os
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv,
+                                              'lib/python3.3/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-    execfile(virtualenv, dict(__file__=virtualenv))
+    exec(compile(open(virtualenv).read(), virtualenv, 'exec'),
+         dict(__file__=virtualenv))
+    # execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
-# 
+#
 
 from app import app as application
