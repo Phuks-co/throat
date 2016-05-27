9e1920840cde5ebc27f3898abb079b7df500c06b
Ramiro Bou <bouramiro@gmail.com>
2016-05-27 01:39:24 -0300

    Adding the correct name to the setup.py and enabling hot deploy on openshift

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Throat',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+      description='Throat discussion platform')
