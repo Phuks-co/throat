5367f003e4db852542f71b0a7cd0892dc67f2cb0
Ramiro Bou <bouramiro@gmail.com>
2016-05-29 01:05:05 -0300

    Updating config for postgres.

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 # We're using a sqlite database for testing.
 # In production we _should_ use mysql or something else
-SQLALCHEMY_DATABASE_URI = os.getenv('THROAT_DB') or 'sqlite:////tmp/test.db'
-SECRET_KEY = os.getenv('THROAT_SECRET') or "WTF!!"
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:////tmp/test.db'
+SECRET_KEY = os.getenv('THROAT_SECRET') or \
+             'yS\x1c\x88\xd7\xb5\xb0\xdc\t:kO\r\xf0D{"Y\x1f\xbc^\xad'
 
 WTF_CSRF_ENABLED = True
 WTF_CSRF_SECRET_KEY = SECRET_KEY
