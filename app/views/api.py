70b775160f81f1472196a6a9d34a0ac5f4b359b0
pembo210 <pembo210@gmail.com>
2016-06-15 11:31:12 -0400

    cleanup some of my mess

--- app/views/api.py
+++ app/views/api.py
@@ -6,10 +6,10 @@ import bcrypt
 from flask import Blueprint, jsonify, abort
 from sqlalchemy import func
 from ..models import db, User, Sub, SubPost, Message, SubPostComment
-#from ..models import SubPostVote, SubMetadata, SubPostMetadata, SubStylesheet
-#from ..models import UserMetadata, UserBadge
-#from flask_login import login_user, login_required, logout_user, current_user
-#from ..misc import SiteUser
+# from ..models import SubPostVote, SubMetadata, SubPostMetadata, SubStylesheet
+# from ..models import UserMetadata, UserBadge
+from flask_login import login_user, login_required, logout_user, current_user
+# from ..misc import SiteUser
 
 api = Blueprint('api', __name__)
 
@@ -18,23 +18,23 @@ api = Blueprint('api', __name__)
 @api.route("/api/v1/status", methods=['GET'])
 def status():
     """ status endpoint """
-    data = {'success': bool(1), 'version': '1.0'}
+    data = {'status': 'active', 'version': '1.0'}
     resp = jsonify(data)
     resp.status_code = 200
     return resp
 
 
 @api.route("/api/v1/u/<user>", methods=['GET'])
+@login_required
 def view_user(user):
     """ Get user info """
     user = User.query.filter_by(name=user).first()
     if not user:
         abort(404)
     else:
-        data = {'success': bool(1), 'user': {
-                'name': user.name,
+        data = {'name': user.name,
                 'joindate': user.joindate,
-                'status': user.status}}
+                'status': user.status}
         resp = jsonify(data)
         resp.status_code = 200
         return resp
@@ -47,14 +47,13 @@ def view_sub(sub):
     if not sub.name != sub:
         abort(404)
     else:
-        data = {'success': bool(1), 'sub': {
-                'name': sub.name,
+        data = {'name': sub.name,
                 'title': sub.title,
                 'created': sub.getSubCreation,
                 'posts': sub.getSubPostCount,
                 'mod': sub.getModName,
                 'status': sub.status,
-                'nsfw': sub.isNSFW}}
+                'nsfw': sub.isNSFW}
         resp = jsonify(data)
         resp.status_code = 200
         return resp
@@ -67,7 +66,7 @@ def view_post(sub, pid):
     if not post or post.sub.name != sub:
         abort(404)
     else:
-        data = {'success': bool(1), 'post': {
+        data = {
                 'id': post.pid,
                 'title': post.title,
                 'link': post.link,
@@ -75,7 +74,7 @@ def view_post(sub, pid):
                 'user': post.user.name,
                 'posted': post.posted,
                 'ptype': post.ptype,
-                'votes': post.voteCount}}
+                'votes': post.voteCount}
         resp = jsonify(data)
         resp.status_code = 200
         return resp
@@ -83,7 +82,7 @@ def view_post(sub, pid):
 
 @api.errorhandler(404)
 def not_found(error):
-        data = {'success': bool(0), 'message': 'not found'}
+        data = {'error': 'not found'}
         resp = jsonify(data)
         resp.status_code = 404
         return resp
