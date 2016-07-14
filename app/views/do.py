25c11eb20dedb53ece6d653507be480aa15d14c6
Ramiro Bou <bouramiro@gmail.com>
2016-07-14 15:06:19 -0300

    Fixing profile editing for admins. Dammit, pembo.

--- app/views/do.py
+++ app/views/do.py
@@ -132,7 +132,7 @@ def edit_user(user):
     if not user:
         return json.dumps({'status': 'error',
                            'error': ['User does not exist']})
-    if current_user.get_id() == user.uid or current_user.is_admin():
+    if current_user.get_id() == user.uid or not current_user.is_admin():
         abort(403)
 
     form = EditUserForm()
