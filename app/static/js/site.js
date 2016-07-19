b26f421b77ef4cdde8add5800436f78544d26869
Ramiro Bou <bouramiro@gmail.com>
2016-07-19 20:35:34 -0300

    Temporally disabling autosave.

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -438,7 +438,7 @@ $(document).ready(function() {
       // Here we append it _next_ to the div that is holding the reply button
       $(e.target).parent().parent().after().after().append(x);
       // Here we hackishly get the textarea and initialize the MDE
-      var l = new SimpleMDE({element: $(x[0]).children('.CommentContent').children('#comment')[0], autoDownloadFontAwesome: false, spellChecker: false, autosave: {enabled: true, unique_id: "createcomment",}});
+      var l = new SimpleMDE({element: $(x[0]).children('.CommentContent').children('#comment')[0], autoDownloadFontAwesome: false, spellChecker: false, autosave: {enabled: false, unique_id: "createcomment",}});
       //$(e.target).parent().next().insertBefore('<span class="close">×</span>');
 
       // Here we hide the reply button...
