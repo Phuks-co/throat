fb394e3578ec199b9ede18ab870db49d138dd096
Ramiro Bou <bouramiro@gmail.com>
2016-12-14 16:56:33 -0300

    Properly returning Response object when debug mode is not enabled

--- app/__init__.py
+++ app/__init__.py
@@ -132,7 +132,7 @@ def before_request():
 def after_request(response):
     """ Called after the request is processed. Used to time the request """
     if not app.debug:
-        return  # We won't do this if we're in production mode
+        return response  # We won't do this if we're in production mode
     diff = time.time() - g.start
     diff = int(diff * 1000)
     if app.debug:
