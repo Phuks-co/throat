0632522a4f6e5b6b748cbe536022a04fd59727df
pembo210 <pembo210@gmail.com>
2016-12-18 08:05:34 -0500

    apparently some links are really long. Now im done, I swear

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -1064,7 +1064,7 @@ function vineID(url) {
 	}
 }
 function gfycatID(url) {
-  var match = url.match(/^https:\/\/gfycat\.com\/([a-zA-Z0-9]{1,20})$/);
+  var match = url.match(/^https:\/\/gfycat\.com\/([a-zA-Z0-9]{1,60})$/);
   if (match){
     return match[1];
 	}
