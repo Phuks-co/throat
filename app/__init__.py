b178d7a58a763ed51837ca3adbdfd2f3c3ec4d33
pembo210 <pembo210@gmail.com>
2017-01-12 17:26:56 -0500

    User guide super rough

--- app/__init__.py
+++ app/__init__.py
@@ -377,6 +377,12 @@ def donate():
     return render_template('donate.html')
 
 
+@app.route("/userguide")
+def userguide():
+    """ User Guide page """
+    return render_template('userguide.html')
+
+
 @app.route("/subs", defaults={'page': 1})
 @app.route("/subs/<int:page>")
 def view_subs(page):
