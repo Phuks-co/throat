4895f7fe3c942d4c63f4b7d3e4ced82971a1805c
Ramiro Bou <bouramiro@gmail.com>
2017-01-24 19:45:28 -0300

    Creating a script that deletes all subs (and references) that after 24 hours from their creation have no content. Closes T204

---
+++ clean_subs.py
@@ -0,0 +1,25 @@
+import MySQLdb
+import MySQLdb.cursors
+import config
+
+rv = MySQLdb.connect(host=config.DB_HOST,
+                         user=config.DB_USER,
+                         passwd=config.DB_PASSWD,
+                         db=config.DB_NAME,
+                         cursorclass=MySQLdb.cursors.DictCursor,
+                         use_unicode=True,
+                         charset="utf8")
+c = rv.cursor()
+
+c.execute('SELECT sm.value AS creation, g.sid, COUNT(m.pid) AS posts FROM sub AS g LEFT JOIN sub_post AS m ON g.sid = m.sid LEFT JOIN sub_metadata AS sm ON sm.sid = g.sid AND sm.key = \'creation\' GROUP BY g.sid HAVING posts = 0 AND CAST(creation AS datetime) < NOW() - INTERVAL 1 DAY')
+subs = c.fetchall()
+
+for sub in subs:
+    c.execute('DELETE FROM `sub_metadata` WHERE `sid`=%s', (sub['sid'],))
+    c.execute('DELETE FROM `sub_stylesheet` WHERE `sid`=%s', (sub['sid'],))
+    c.execute('DELETE FROM `sub_subscriber` WHERE `sid`=%s', (sub['sid'],))
+    c.execute('DELETE FROM `sub_flair` WHERE `sid`=%s', (sub['sid'],))
+    c.execute('DELETE FROM `sub_log` WHERE `sid`=%s', (sub['sid'],))
+    c.execute('DELETE FROM `sub` WHERE `sid`=%s', (sub['sid'],))
+
+    rv.commit()
