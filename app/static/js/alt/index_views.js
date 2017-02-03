31edd70c657a99d009f48f320379a498a9aebc07
pembo210 <pembo210@gmail.com>
2017-02-03 17:54:17 -0500

    remove +num from top 5

--- app/static/js/alt/index_views.js
+++ app/static/js/alt/index_views.js
@@ -12,7 +12,7 @@ function gettop5sidebar (posts) {
   list = [];
   for (var i in posts) {
     list.push(m('li', m('span.TPTitle'), m('a', {href: '/s/' + posts[i].sub.name + '/' + posts[i].pid, oncreate: m.route.link}, posts[i].title),
-                      m('div.TPlocal', '+' + posts[i].score + ', ', m('time-ago', {datetime: posts[i].posted}), ' in ', m('a', {href: '/s/' + posts[i].sub.name , oncreate: m.route.link}, posts[i].sub.name)  )));
+                      m('div.TPlocal', m('time-ago', {datetime: posts[i].posted}), ' in ', m('a', {href: '/s/' + posts[i].sub.name , oncreate: m.route.link}, posts[i].sub.name)  )));
   }
   return list;
 }
