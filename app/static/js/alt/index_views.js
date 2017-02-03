b9e7ea034ed24899263d4d965cd5831d55ca33f0
Ramiro Bou <bouramiro@gmail.com>
2017-02-03 20:05:54 -0300

    Fixing it

--- app/static/js/alt/index_views.js
+++ app/static/js/alt/index_views.js
@@ -11,8 +11,8 @@
 function gettop5sidebar (posts) {
   list = [];
   for (var i in posts) {
-    list.push(m('li', m('span.TPTitle', m('a', {href: '/s/' + posts[i].sub.name + '/' + posts[i].pid, oncreate: m.route.link}, posts[i].title)),
-                      m('div.TPlocal', m('time-ago', {datetime: posts[i].posted}), ' in ', m('a', {href: '/s/' + posts[i].sub.name , oncreate: m.route.link}, posts[i].sub.name)  )));
+    list.push(m('li', m('div.TPTitle', m('a', {href: '/s/' + posts[i].sub.name + '/' + posts[i].pid, oncreate: m.route.link}, posts[i].title)),
+                      m('div.TPlocal', m('span.TPscore', posts[i].score), ' ', m('time-ago', {datetime: posts[i].posted}), ' in ', m('a', {href: '/s/' + posts[i].sub.name , oncreate: m.route.link}, posts[i].sub.name)  )));
   }
   return list;
 }
