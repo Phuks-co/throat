f021c3751da7bd53ace7ba830a6f6e566f80612d
Ramiro Bou <bouramiro@gmail.com>
2017-07-03 17:49:52 -0300

    Adding time-elements

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,9 +1,11 @@
+import './ext/CustomElements.min.js';
 import 'purecss/build/base.css';
 import 'purecss/build/forms.css';
 import 'purecss/build/buttons.css';
 import 'purecss/build/grids.css';
 import 'purecss/build/grids-responsive.css';
 import 'purecss/build/menus.css';
+import 'time-elements/time-elements.js';
 
 require('../css/main.css');
 require('../css/dark.css');
