f5887e1d3da59a9d94b16c7ee9c4bf739877ee90
Ramiro Bou <bouramiro@gmail.com>
2018-02-25 12:06:52 -0300

    Fixing editing empty posts and setting minimum height for post editing textarea

--- app/static/js/Post.js
+++ app/static/js/Post.js
@@ -131,6 +131,9 @@ u.addEventForChild(document, 'click', '.edit-post', function(e, qelem){
     this.parentNode.innerHTML = '<a class="edit-post">edit</a>';
   };
   var h = elem.clientHeight-6;
+  if(h<100){
+    h = 100;
+  }
   elem.innerHTML = '<div id="editpost" class="cwrap markdown-editor"><textarea style="height: ' + h + 'px">' + document.getElementById('post-source').innerHTML + '</textarea></div><div style="display:none" class="error"></div><button class="pure-button pure-button-primary button-xsmall btn-editpost" data-pid="' + qelem.getAttribute('data-pid') +'">Save changes</button> <button class="pure-button button-xsmall btn-preview" data-pvid="editpost" >Preview</button><div class="cmpreview canclose" style="display:none;"><h4>Comment preview</h4><span class="closemsg">&times;</span><div class="cpreview-content"></div></div>';
   qelem.replaceWith(back);
   initializeEditor(document.getElementById('editpost'));
