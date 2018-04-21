4444424a9d86e48baa9808a4429fbab073ee76f9
Ramiro Bou <bouramiro@gmail.com>
2018-04-21 00:15:00 -0300

    Disallow posting on /s/all

--- app/views/do.py
+++ app/views/do.py
@@ -573,6 +573,8 @@ def create_post():
         sub = db.get_sub_from_name(form.sub.data)
         if not sub:
             return render_template('createpost.html', txtpostform=form, error="Sub does not exist")
+        if sub.name.lower() in ('all', 'new', 'hot', 'top', 'admin', 'home'):
+            return render_template('createpost.html', txtpostform=form, error="You cannot post in this sub.")
         if current_user.is_subban(sub):
             return render_template('createpost.html', txtpostform=form, error="You're banned from posting on this sub")
         if misc.isRestricted(sub) and not current_user.is_mod(sub['sid']):
