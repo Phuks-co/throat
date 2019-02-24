2b8c6f2e0d2ae7b83550bd8bdde5f5c5e67ce21c
Ramiro Bou <bouramiro@gmail.com>
2019-02-23 22:58:01 -0300

    API: Adding getPost

--- app/views/api.py
+++ app/views/api.py
@@ -1,9 +1,4 @@
-""" API endpoint.
-Some rules we should follow:
- - Always return JSON
- - Always return a 'status' key ({"status": "ok/error"})
- - If status is "error", return an "errors" _list_
-"""
+""" API endpoints. """
 
 from datetime import datetime, timedelta
 from flask import Blueprint, jsonify, request, render_template, g, current_app
@@ -227,3 +222,35 @@ def getPostList(target, sort, page):
         np.append(p)
 
     return jsonify(status='ok', posts=np)
+
+
+@api.route('/api/getPost/<int:pid>', methods=['GET'])
+def getPost(pid):
+
+    base_query = SubPost.select(SubPost.nsfw, SubPost.content, SubPost.pid, SubPost.title, SubPost.posted, SubPost.score, SubPost.deleted,
+                                SubPost.thumbnail, SubPost.link, User.name.alias('user'), Sub.name.alias('sub'), SubPost.flair, SubPost.edited,
+                                SubPost.comments, SubPost.ptype, User.status.alias('userstatus'), User.uid, SubPost.upvotes, SubPost.downvotes)
+    base_query = base_query.join(User, JOIN.LEFT_OUTER).switch(SubPost).join(Sub, JOIN.LEFT_OUTER)
+
+    post = base_query.where(SubPost.pid == pid).dicts()
+
+    if len(post) == 0:
+        return jsonify(status="error", error="Post does not exist")
+    
+    post = post[0]
+    post['deleted'] = True if post['deleted'] != 0 else False
+    
+    if post['deleted']:  # Clear data for deleted posts
+        post['content'] = None
+        post['link'] = None
+        post['uid'] = None
+        post['user'] = '[Deleted]'
+        post['thumbnail'] = None
+        post['edited'] = None
+    
+    if post['userstatus'] == 10:
+        post['user'] = '[Deleted]'
+        post['uid'] = None
+    del post['userstatus']
+
+    return jsonify(status='ok', post=post)
