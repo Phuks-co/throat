10da088e5537772a243ba62d7a7302e6a002e33e
Ramiro Bou <bouramiro@gmail.com>
2019-06-22 19:49:37 -0300

    migrate.py: Adding parameter to create a new migration

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -4,8 +4,18 @@ from peewee_migrate import Router
 from playhouse.db_url import connect
 import config
 import os
+import argparse
+
 config.TESTING = False
 database = connect(config.DATABASE_URL)
 router = Router(database, migrate_dir='../app/migrations' if os.getcwd().endswith('scripts') else 'app/migrations')
 
-router.run()
+parser = argparse.ArgumentParser(description='Apply or manage database migrations.')
+parser.add_argument('-c', '--create', metavar='NAME', help='Creates a new migration')
+
+args = parser.parse_args()
+
+if args.create:
+    router.create(args.create)
+else:
+    router.run()
