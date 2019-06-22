807556d298ffc93b76672d0d8a12e7abecd76fa9
Ramiro Bou <bouramiro@gmail.com>
2019-06-22 19:43:28 -0300

    Allowing execution of scripts from the root throat directory

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -3,8 +3,9 @@ import __fix
 from peewee_migrate import Router
 from playhouse.db_url import connect
 import config
+import os
 config.TESTING = False
 database = connect(config.DATABASE_URL)
-router = Router(database, migrate_dir='../app/migrations')
+router = Router(database, migrate_dir='../app/migrations' if os.getcwd().endswith('scripts') else 'app/migrations')
 
 router.run()
