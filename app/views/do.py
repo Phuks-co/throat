25a252ca74a685eac02e93d6a28beb7d27332fab
Ramiro Bou <bouramiro@gmail.com>
2019-09-17 22:58:43 -0300

    Fixing saving PMs too

--- app/views/do.py
+++ app/views/do.py
@@ -1286,7 +1286,7 @@ def save_pm(mid):
     """ Save/Archive PM """
     try:
         message = Message.get(Message.mid == mid)
-        if message.receivedby != current_user.uid:
+        if message.receivedby_id != current_user.uid:
             return jsonify(status='error', error="Message does not exist")
         
         message.mtype = 9
