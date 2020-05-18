f76868e91715d14e56ab65498ad7f0f6d9134985
globalistas <64347618+globalistas@users.noreply.github.com>
2020-05-18 20:43:43 +0200

    allow subs with diacritics

--- app/misc.py
+++ app/misc.py
@@ -53,7 +53,7 @@ engine = Engine(
 redis = redis.from_url(config.app.redis_url)
 
 # Regex that matches VALID user and sub names
-allowedNames = re.compile("^[a-zA-Z0-9_-]+$")
+allowedNames = re.compile("^[a-zA-ZÀ-ž0-9_-]+$")
 WHITESPACE = "\u0009\u000A\u000B\u000C\u000D\u0020\u0085\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007" \
              "\u2008\u2009\u200a\u200b\u2029\u202f\u205f\u3000\u180e\u200b\u200c\u200d\u2060\ufeff\u00AD\ufffc "
 
