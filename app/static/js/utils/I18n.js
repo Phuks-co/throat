015040fd254141b0aa924418934378ef455810b8
Ramiro Bou <bouramiro@gmail.com>
2020-05-31 11:42:30 -0300

    Adding Russian translation to Javascript i18n

--- app/static/js/utils/I18n.js
+++ app/static/js/utils/I18n.js
@@ -1,6 +1,7 @@
 // When adding new languages, add them here.
 let languages = {
     'es': require('../../../translations/es/LC_MESSAGES/messages.po'),
+    'ru': require('../../../translations/ru/LC_MESSAGES/messages.po'),
 };
 
 // Taken from gettext.js
