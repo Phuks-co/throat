edc4039ba54da2afb4374b91534c951e34d1f14c
Ramiro Bou <bouramiro@gmail.com>
2020-06-04 20:04:22 -0300

    Fixing poll date picker on mobile. Closes #18

--- app/static/js/Poll.js
+++ app/static/js/Poll.js
@@ -38,12 +38,15 @@ u.addEventForChild(document, 'click', '.poll-del-opt', function(e, qelem){
 
 
 u.sub('#closetime', 'click', function(e){
+    console.log('wooo')
     if(this.checked){
         document.getElementById('closetime_date').removeAttribute('disabled');
         document.querySelector('.date-picker-future.input').removeAttribute('disabled');
+        document.querySelector('.date-picker-future.flatpickr-mobile').removeAttribute('disabled');
     }else{
         document.getElementById('closetime_date').setAttribute('disabled', true);
         document.querySelector('.date-picker-future.input').setAttribute('disabled', true);
+        document.querySelector('.date-picker-future.flatpickr-mobile').setAttribute('disabled', true);
         document.getElementById('closetime_date').value = '';
         document.querySelector('.date-picker-future.input').value = '';
     }
