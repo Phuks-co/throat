dc6362dc8e4febfffd36f46da760c1952ad74d87
Mary Kate <mk@marykatefain.com>
2020-07-04 16:54:23 -0500

    code cleanup

--- app/misc.py
+++ app/misc.py
@@ -1171,15 +1171,28 @@ def getSubData(sid, simple=False, extra=False):
 def getSubReports(sid):
     reports = {'open': [], 'closed': []}
 
-    open_post_reports = SubPostReport.select().join(SubPost).where(SubPost.sid == sid)      .where(SubPostReport.open == True)
+    open_post_reports = SubPostReport.select() \
+        .join(SubPost) \
+        .where(SubPost.sid == sid) \
+        .where(SubPostReport.open == True)
 
-    open_post_comment_reports = SubPostCommentReport.select().join(SubPostComment).where(SubPostComment.cid == SubPostCommentReport.cid).join(SubPost).where(SubPost.pid == SubPostComment.pid).join(Sub).where(SubPost.sid == sid).where(SubPostCommentReport.open == True)
+    open_post_comment_reports = SubPostCommentReport.select() \
+        .join(SubPostComment).where(SubPostComment.cid == SubPostCommentReport.cid) \
+        .join(SubPost).where(SubPost.pid == SubPostComment.pid) \
+        .join(Sub).where(SubPost.sid == sid) \
+        .where(SubPostCommentReport.open == True)
 
     reports['open'] = open_post_reports + open_post_comment_reports
 
-    closed_post_reports = SubPostReport.select().join(SubPost).where(SubPost.sid == sid)      .where(SubPostReport.open == False)
+    closed_post_reports = SubPostReport.select() \
+        .join(SubPost).where(SubPost.sid == sid) \
+        .where(SubPostReport.open == False)
 
-    closed_post_comment_reports = SubPostCommentReport.select().join(SubPostComment).where(SubPostComment.cid == SubPostCommentReport.cid).join(SubPost).where(SubPost.pid == SubPostComment.pid).join(Sub).where(SubPost.sid == sid).where(SubPostCommentReport.open == False)
+    closed_post_comment_reports = SubPostCommentReport.select() \
+        .join(SubPostComment).where(SubPostComment.cid == SubPostCommentReport.cid) \
+        .join(SubPost).where(SubPost.pid == SubPostComment.pid) \
+        .join(Sub).where(SubPost.sid == sid) \
+        .where(SubPostCommentReport.open == False)
 
     reports['closed'] = closed_post_reports + closed_post_comment_reports
 
