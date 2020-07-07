c7676c95ff8f1358b4da2cc26165a0c5e9d6dbcf
Mary Kate <mk@marykatefain.com>
2020-07-07 18:39:04 -0500

    refactor getReports works in template but nothing else

--- app/views/mod.py
+++ app/views/mod.py
@@ -8,7 +8,7 @@ from flask_babel import _
 from .. import misc
 from ..models import UserMetadata, User, Sub, SubPost, SubPostComment
 from ..models import User, Sub, SubMod, SubPost, SubPostComment, UserMetadata, SubPostReport, SubPostCommentReport
-from ..misc import engine, getSubReports, getModSubs
+from ..misc import engine, getSubReports, getModSubs, getReports
 
 bp = Blueprint('mod', __name__)
 
@@ -56,63 +56,9 @@ def reports(page):
     if not (SubMod.select().where(SubMod.user == current_user.uid) or current_user.can_admin):
         abort(404)
 
-    mod_subs = getModSubs(current_user.uid, 1)
+    reports = getReports('mod', 'open', page)
 
-    # Get all reports on posts and comments, filter by "open",
-    # then filter by subs current user can Mod
-    # also returns count of all open reports
-    Reported = User.alias()
-    posts_q = SubPostReport.select(
-        Value('post').alias('type'),
-        SubPostReport.id,
-        SubPostReport.pid,
-        Value(None).alias('cid'),
-        User.name.alias('reporter'),
-        Reported.name.alias('reported'),
-        SubPostReport.datetime,
-        SubPostReport.reason,
-        SubPostReport.open,
-        Sub.name.alias('sub')
-    ).join(User, on=User.uid == SubPostReport.uid) \
-        .switch(SubPostReport).join(SubPost).join(Sub).join(SubMod) \
-        .where(SubMod.user == current_user.uid) \
-        .join(Reported, on=Reported.uid == SubPost.uid)
-
-    open_posts_q = posts_q.where(SubPostReport.open == True)
-    closed_posts_q = posts_q.where(SubPostReport.open == False)
-
-
-    comments_q = SubPostCommentReport.select(
-        Value('comment').alias('type'),
-        SubPostCommentReport.id,
-        SubPostComment.pid,
-        SubPostCommentReport.cid,
-        User.name.alias('reporter'),
-        Reported.name.alias('reported'),
-        SubPostCommentReport.datetime,
-        SubPostCommentReport.reason,
-        SubPostCommentReport.open,
-        Sub.name.alias('sub')
-    ).join(User, on=User.uid == SubPostCommentReport.uid) \
-        .switch(SubPostCommentReport).join(SubPostComment).join(SubPost).join(Sub).join(SubMod) \
-        .where(SubMod.user == current_user.uid) \
-        .join(Reported, on=Reported.uid == SubPostComment.uid)
-
-    open_comments_q = comments_q.where(SubPostCommentReport.open == True)
-    closed_comments_q = comments_q.where(SubPostCommentReport.open == False)
-
-    open_query = open_posts_q | open_comments_q
-    open_query = open_query.order_by(open_query.c.datetime.desc())
-    open_report_count = open_query.count()
-
-    closed_query = closed_posts_q | closed_comments_q
-    closed_query = closed_query.order_by(closed_query.c.datetime.desc())
-    closed_report_count = closed_query.count()
-
-    open_query = open_query.paginate(page, 50)
-
-    return engine.get_template('mod/reports.html').render(
-        {'open_reports': list(open_query.dicts()), 'open_report_count': str(open_report_count), 'closed_report_count': str(closed_report_count)})
+    return engine.get_template('mod/reports.html').render({'reports': reports})
 
 
 @bp.route("/reports/closed", defaults={'page': 1})
