4c40f98c6826f4caa0c9a698a707465f5e9e6a3d
Happy River <happyriver93@runbox.com>
2020-07-10 13:19:19 -0700

    Add a comment and docstring to example test

--- test/views/test_auth.py
+++ test/views/test_auth.py
@@ -1,5 +1,9 @@
 from test.fixtures import client
 
+# This is intended as a sample first test, not a real test of login
+# functionality. All it does is check that somewhere in the login page
+# is the string "password".
 def test_login(client):
+    """The login page mentions passwords."""
     rv = client.get("/login")
     assert b"password" in rv.data
