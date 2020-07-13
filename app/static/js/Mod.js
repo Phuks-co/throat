e96ef870d1bd503747198ce7c1f5085361adf8bb
Mary Kate <mk@marykatefain.com>
2020-07-13 10:00:28 -0500

    not working but started close related posts button

--- app/static/js/Mod.js
+++ app/static/js/Mod.js
@@ -7,7 +7,6 @@ u.addEventForChild(document, 'click', '.close-report', function (e, qelem) {
   const errorbox = document.querySelector('.error');
 
   let action = qelem.getAttribute('data-action');
-  console.log('ACTION:' + action)
   let id = qelem.getAttribute('data-id');
   let type = qelem.getAttribute('data-type');
 
@@ -15,35 +14,79 @@ u.addEventForChild(document, 'click', '.close-report', function (e, qelem) {
     let uri = '/do/report/close_comment_report/' + id + '/' + action;
     u.post(uri, {},
       function (data) {
-          if (data.status != "ok") {
-            errorbox.style.display = 'block';
-            errorbox.innerHTML = _('Error:') + data.error;
-          } else {
-              window.location.reload();
-          }
-      }, function () {
+        if (data.status != "ok") {
           errorbox.style.display = 'block';
-          errorbox.innerHTML = _('Could not contact the server');
+          errorbox.innerHTML = _('Error:') + data.error;
+        } else {
+          window.location.reload();
+        }
+      }, function () {
+        errorbox.style.display = 'block';
+        errorbox.innerHTML = _('Could not contact the server');
       });
   }
   else {
     let uri = '/do/report/close_post_report/' + id + '/' + action;
     u.post(uri, {},
       function (data) {
-          if (data.status != "ok") {
-            errorbox.style.display = 'block';
-            errorbox.innerHTML = _('Error:') + data.error;
-          } else {
-              window.location.reload();
-          }
-      }, function () {
+        if (data.status != "ok") {
           errorbox.style.display = 'block';
-          errorbox.innerHTML = _('Could not contact the server');
+          errorbox.innerHTML = _('Error:') + data.error;
+        } else {
+          window.location.reload();
+        }
+      }, function () {
+        errorbox.style.display = 'block';
+        errorbox.innerHTML = _('Could not contact the server');
       });
-    }
+  }
 });
 
 u.addEventForChild(document, 'click', '.banuserbutton', function (e, qelem) {
   let form = document.getElementById('report-ban-user-form');
   form.style.display = 'block';
+});
+
+
+u.addEventForChild(document, 'click', '.close-related-reports', function (e, qelem) {
+  const errorbox = document.querySelector('.error');
+
+  let action = qelem.getAttribute('data-action');
+  let reports = qelem.getAttribute('data-reports');
+  let type = qelem.getAttribute('data-type');
+
+  if (type == "comment") {
+    reports.forEach(function(report) {
+      let uri = '/do/report/close_comment_report/' + report['id'] + '/' + action;
+      u.post(uri, {},
+        function (data) {
+          if (data.status != "ok") {
+            errorbox.style.display = 'block';
+            errorbox.innerHTML = _('Error:') + data.error;
+          } else {
+            window.location.reload();
+          }
+        }, function () {
+          errorbox.style.display = 'block';
+          errorbox.innerHTML = _('Could not contact the server');
+        });
+    });
+  }
+  else {
+    reports.forEach(function(report) {
+      let uri = '/do/report/close_post_report/' + report['id'] + '/' + action;
+      u.post(uri, {},
+        function (data) {
+          if (data.status != "ok") {
+            errorbox.style.display = 'block';
+            errorbox.innerHTML = _('Error:') + data.error;
+          } else {
+            window.location.reload();
+          }
+        }, function () {
+          errorbox.style.display = 'block';
+          errorbox.innerHTML = _('Could not contact the server');
+        });
+    });
+  }
 });
