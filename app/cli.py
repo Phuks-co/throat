26e6697fbca625fd73671d027806059dcef47fae
Happy River <happyriver93@runbox.com>
2020-07-31 07:45:23 -0700

    Start our new command line interface with 'recount subscribers'

---
+++ app/cli.py
@@ -0,0 +1,30 @@
+import click
+from flask import current_app
+from flask.cli import AppGroup
+from .models import User, Sub, SubSubscriber
+
+recount = AppGroup('recount')
+
+
+@recount.command()
+@click.option('--save/--dry-run', default=True,
+              help='Use --save (the default) to fix the counts, or --dry-run to just print them.')
+def subscribers(save):
+    """Update subscriber counts for all the subs."""
+    if save:
+        print('Name                             Before   After')
+    else:
+        print('Name                            Current Correct')
+
+    subs = Sub.select(Sub.sid, Sub.name, Sub.subscribers).order_by(Sub.name)
+    for sub in subs:
+        count = SubSubscriber.select().where((SubSubscriber.sid == sub.sid) &
+                                             (SubSubscriber.status == 1)).count()
+        print(f'{sub.name:32}{sub.subscribers:7}{count:8}')
+        if save:
+            Sub.update(subscribers=SubSubscriber.select().where(
+                (SubSubscriber.sid == sub.sid) & (SubSubscriber.status == 1)).count()).where(
+                    Sub.sid == sub.sid).execute()
+
+
+commands = [recount]
--- throat.py
+++ throat.py
@@ -2,8 +2,22 @@
 """ From here we start the app in debug mode. """
 from gevent import monkey
 monkey.patch_all()
+import click
 from app import create_app, socketio  # noqa
+from app.cli import commands
+
 app = create_app()
 
+@click.group(invoke_without_command=True)
+@click.pass_context
+def run(ctx):
+    if ctx.invoked_subcommand is None:
+        socketio.run(app, debug=app.config.get('DEBUG'), host=app.config.get('HOST'))
+
+
+for command in commands:
+    run.add_command(command)
+
+
 if __name__ == "__main__":
-    socketio.run(app, debug=app.config.get('DEBUG'), host=app.config.get('HOST'))
+    run()
