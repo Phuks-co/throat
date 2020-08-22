5a5099134cba8bded79be170cbf5182ca1eada26
Happy River <happyriver93@runbox.com>
2020-08-22 07:15:25 -0700

    Cache rendered error templates

--- app/views/errors.py
+++ app/views/errors.py
@@ -1,7 +1,8 @@
 """ Pages to be migrated to a wiki-like system """
 from flask import Blueprint, request, redirect, url_for, jsonify, current_app
-from ..misc import engine, logger
+from ..misc import engine, logger, ensure_locale_loaded, get_locale
 from ..forms import LoginForm
+from ..caching import cache
 
 bp = Blueprint('errors', __name__)
 
@@ -15,7 +16,7 @@ def unauthorized(error):
 @bp.app_errorhandler(403)
 def forbidden_error(error):
     """ 403 Forbidden """
-    return engine.get_template('errors/403.html').render(), 403
+    return render_error_template('errors/403.html'), 403
 
 
 @bp.app_errorhandler(404)
@@ -25,19 +26,19 @@ def not_found(error):
         if request.path.startswith('/api/v3'):
             return jsonify(msg="Method not found or not implemented"), 404
         return jsonify(status='error', error='Method not found or not implemented'), 404
-    return engine.get_template('errors/404.html').render({}), 404
+    return render_error_template('errors/404.html'), 404
 
 
 @bp.app_errorhandler(417)
 def forbidden_error(error):
     """ 418 I'm a teapot """
-    return engine.get_template('errors/417.html').render({}), 418
+    return render_error_template('errors/417.html'), 418
 
 
 @bp.app_errorhandler(429)
 def too_many_requests_error(error):
     """ 429 Too many requests """
-    return engine.get_template('errors/429.html').render({}), 429
+    return render_error_template('errors/429.html'), 429
 
 
 @bp.app_errorhandler(500)
@@ -52,4 +53,16 @@ def server_error(error):
             return jsonify(msg="Internal error"), 500
         return jsonify(status='error', error='Internal error'), 500
 
-    return engine.get_template('errors/500.html').render({}), 500
+    return render_error_template('errors/500.html'), 500
+
+
+def render_error_template(template):
+    ensure_locale_loaded()
+    lang = get_locale()
+    daynight_cookie = request.cookies.get("dayNight")
+    return _render_error_template(template, lang, daynight_cookie)
+
+
+@cache.memoize(300)
+def _render_error_template(template, lang, daynight_cookie):
+    return engine.get_template(template).render({})
