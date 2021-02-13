9f8bf34b63f4a97a42f057f32458cae9f1a9d912
globalistas <64347618+globalistas@users.noreply.github.com>
2021-02-13 21:45:03 +0100

    move update.sh to root

--- scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $(git rev-parse --show-toplevel)
-git pull
-pip install -r requirements.txt
-npm install
-npm run build
-./scripts/genmo.sh
-./throat.py migration apply
-killall -HUP gunicorn
