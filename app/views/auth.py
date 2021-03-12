a5457f4c1cdccc6711b0d9731455e904884d7006
globalistas <64347618+globalistas@users.noreply.github.com>
2021-03-12 20:35:56 +0100

    testing sendmail fix v2

--- app/views/auth.py
+++ app/views/auth.py
@@ -224,8 +224,8 @@ def register():
     text_content = user.name + "\n" + ip + "\n" + regdate + "\n" + useragent
     try:
         send_email(config.mail.default_to, "New registration", text_content, "")
-    except:
-        print("boo")
+    except Exception as ex:
+        print(ex)
     if email_validation_is_required():
         send_login_link_email(user)
         return redirect(url_for("auth.confirm_registration"))
