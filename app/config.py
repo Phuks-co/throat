5702a01e79a0dbc8be40ef5c9daa60a3c295a1b6
globalistas <64347618+globalistas@users.noreply.github.com>
2021-03-14 16:13:31 +0100

    adding default_to to config.py #1

--- app/config.py
+++ app/config.py
@@ -74,7 +74,7 @@ cfg_defaults = {  # key => default value
     },
     "auth": {"provider": "LOCAL", "require_valid_emails": False, "keycloak": {}},
     "cache": {"type": "null"},
-    "mail": {},
+    "mail": {"default_to": "example@example5464.com"},
     "storage": {
         "provider": "LOCAL",
         "acl": "private",
