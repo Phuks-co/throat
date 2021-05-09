Phuks-co/throat

commit f841183dc3c87fb34103d6913b4be62c8124f6c9

Add a site configuration page for admins (#333)
* Refactor config Map object to support storing config values in database

* Add admin site configuration page

* Make reference to site log on site configuration page translatable

* Remove site.upload_max_size from the site configuration page

* Add pointer cursor to clickables on site configuration page

* Confirm changes to boolean values on site configuration page

app/misc.py
@@ -572,26 +572,6 @@ def getDefaultSubs_list(ext=False):
     return defaults
 
 
-@cache.memoize(600)
-def enableInviteCode():
-    """ Returns true if invite code is required to register """
-    try:
-        xm = SiteMetadata.get(SiteMetadata.key == "useinvitecode")
-        return False if xm.value == "0" else True
-    except SiteMetadata.DoesNotExist:
-        return False
-
-
-@cache.memoize(600)
-def user_visible_invitations():
-    """ Returns True if users can see who they invited and who invited them. """
-    try:
-        xm = SiteMetadata.get(SiteMetadata.key == "invitations_visible_to_users")
-        return False if xm.value == "0" else True
-    except SiteMetadata.DoesNotExist:
-        return False
-
-
 @cache.memoize(30)
 def getMaxCodes(uid):
     """ Returns how many invite codes a user can create """
@@ -601,15 +581,11 @@ def getMaxCodes(uid):
         )
         return amt.value
     except UserMetadata.DoesNotExist:
-        try:
-            # If there's no setting for the user, use the global setting, but checkk the user's level first
-            minlevel = SiteMetadata.get(SiteMetadata.key == "invite_level")
-            if get_user_level(uid)[0] >= int(minlevel.value):
-                amt = SiteMetadata.get(SiteMetadata.key == "invite_max")
-                return amt.value
-        except SiteMetadata.DoesNotExist:
+        # If there's no setting for the user, use the global setting, but checkk the user's level first
+        if get_user_level(uid)[0] >= config.site.invite_level:
+            return config.site.invite_max
+        else:
             return 0
-    return 0
 
 
 @cache.memoize(30)
@@ -621,7 +597,7 @@ def getInviteCodeInfo(uid):
 
     if not (
         current_user.is_admin()
-        or (user_visible_invitations() and current_user.uid == uid)
+        or (config.invitations_visible_to_users and current_user.uid == uid)
     ):
         return info
 
@@ -1943,7 +1919,6 @@ def get_postmeta_dicts(pids):
 # Log types
 LOG_TYPE_USER = 10
 LOG_TYPE_USER_BAN = 19
-LOG_TYPE_USER_UNBAN = 54
 
 LOG_TYPE_SUB_CREATE = 20
 LOG_TYPE_SUB_SETTINGS = 21
@@ -1955,12 +1930,6 @@ LOG_TYPE_SUB_MOD_REMOVE = 26
 LOG_TYPE_SUB_MOD_INV_CANCEL = 27
 LOG_TYPE_SUB_MOD_INV_REJECT = 28
 LOG_TYPE_SUB_CSS_CHANGE = 29
-LOG_TYPE_SUB_STICKY_ADD = 50
-LOG_TYPE_SUB_STICKY_DEL = 51
-LOG_TYPE_SUB_DELETE_POST = 52
-LOG_TYPE_SUB_DELETE_COMMENT = 53
-LOG_TYPE_SUB_UNDELETE_POST = 58
-LOG_TYPE_SUB_UNDELETE_COMMENT = 59
 
 LOG_TYPE_SUB_TRANSFER = 30
 
@@ -1968,16 +1937,22 @@ LOG_TYPE_ANNOUNCEMENT = 41
 LOG_TYPE_DOMAIN_BAN = 42
 LOG_TYPE_DOMAIN_UNBAN = 43
 LOG_TYPE_UNANNOUNCE = 44
-LOG_TYPE_DISABLE_POSTING = 45
-LOG_TYPE_ENABLE_POSTING = 46
-LOG_TYPE_ENABLE_INVITE = 47
-LOG_TYPE_DISABLE_INVITE = 48
-LOG_TYPE_DISABLE_REGISTRATION = 49
-LOG_TYPE_ENABLE_REGISTRATION = 50
-
+LOG_TYPE_DISABLE_POSTING = 45  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
+LOG_TYPE_ENABLE_POSTING = 46  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
+LOG_TYPE_ENABLE_INVITE = 47  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
+LOG_TYPE_DISABLE_INVITE = 48  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
+LOG_TYPE_DISABLE_REGISTRATION = 49  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
+LOG_TYPE_ENABLE_REGISTRATION = 50  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
+LOG_TYPE_SUB_STICKY_ADD = 50
+LOG_TYPE_SUB_STICKY_DEL = 51
+LOG_TYPE_SUB_DELETE_POST = 52
+LOG_TYPE_SUB_DELETE_COMMENT = 53
+LOG_TYPE_USER_UNBAN = 54
 LOG_TYPE_REPORT_CLOSE = 55
 LOG_TYPE_REPORT_REOPEN = 56
 LOG_TYPE_REPORT_CLOSE_RELATED = 57
+LOG_TYPE_SUB_UNDELETE_POST = 58
+LOG_TYPE_SUB_UNDELETE_COMMENT = 59
 LOG_TYPE_REPORT_POST_DELETED = 60
 LOG_TYPE_REPORT_POST_UNDELETED = 61
 LOG_TYPE_REPORT_COMMENT_DELETED = 62
@@ -1989,10 +1964,11 @@ LOG_TYPE_REPORT_USER_SUB_UNBANNED = 67
 LOG_TYPE_REPORT_NOTE = 68
 LOG_TYPE_EMAIL_DOMAIN_BAN = 69
 LOG_TYPE_EMAIL_DOMAIN_UNBAN = 70
-LOG_TYPE_DISABLE_CAPTCHAS = 71
-LOG_TYPE_ENABLE_CAPTCHAS = 72
+LOG_TYPE_DISABLE_CAPTCHAS = 71  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
+LOG_TYPE_ENABLE_CAPTCHAS = 72  # Use LOG_TYPE_ADMIN_CONFIG_CHANGE instead
 LOG_TYPE_STICKY_SORT_NEW = 73
 LOG_TYPE_STICKY_SORT_TOP = 74
+LOG_TYPE_ADMIN_CONFIG_CHANGE = 75
 
 
 def create_sitelog(action, uid, comment="", link=""):
@@ -2047,17 +2023,10 @@ def is_domain_banned(addr, domain_type):
     return False
 
 
-def captchas_required():
-    try:
-        return SiteMetadata.get(SiteMetadata.key == "require_captchas").value == "1"
-    except SiteMetadata.DoesNotExist:
-        return True
-
-
 def create_captcha():
     """Generates a captcha image.
     Returns a tuple with a token and the base64 encoded image"""
-    if not captchas_required() or config.app.testing:
+    if not config.site.require_captchas or config.app.testing:
         return None
     token = str(uuid.uuid4())
     captchagen = ImageCaptcha(width=250, height=70)
@@ -2108,7 +2077,7 @@ def create_captcha():
 
 
 def validate_captcha(token, response):
-    if config.app.testing or config.app.development or not captchas_required():
+    if config.app.testing or config.app.development or not config.site.require_captchas:
         return True
     cap = rconn.get("cap-" + token)
     if cap:
@@ -2848,8 +2817,8 @@ def slugify(text):
 def logging_init_app(app):
     config = app.config["THROAT_CONFIG"]
     if "logging" in config:
-        logging.config.dictConfig(config.logging)
-        add_context_to_log_records(config.logging)
+        logging.config.dictConfig(config.logging.as_dict())
+        add_context_to_log_records(config.logging.as_dict())
     elif config.app.development or config.app.testing:
         logging.basicConfig(level=logging.DEBUG)
         logging.getLogger(app.logger.name + ".socketio").setLevel(logging.WARNING)

app/__init__.py
@@ -16,7 +16,7 @@ from werkzeug.wrappers import BaseResponse
 
 from .config import Config
 from .forms import LoginForm, LogOutForm, CreateSubForm
-from .models import db_init_app, rconn, User
+from .models import db_init_app, rconn, User, SiteMetadata
 from .auth import auth_provider, email_validation_is_required
 from .views import do, subs as sub, api3, jwt
 from .views.auth import bp as auth
@@ -51,7 +51,7 @@ login_manager.needs_refresh_message = _l("Please reauthenticate to access this p
 
 def create_app(config=None):
     if config is None:
-        config = Config("config.yaml")
+        config = Config("config.yaml", model=SiteMetadata, cache=caching.cache)
     app = Flask(__name__)
     app.jinja_env.cache = {}
     app.config["THROAT_CONFIG"] = config
@@ -75,11 +75,10 @@ def create_app(config=None):
         "connect-src": ["'self'"],
     }
 
-    server_name = config.site.get("server_name")
-    if server_name is not None:
-        csp["connect-src"] += [f"wss://{server_name}"]
+    if "server_name" in config.site.keys():
+        csp["connect-src"] += [f"wss://{config.site.server_name}"]
         if not config.app.force_https:
-            csp["connect-src"] += [f"ws://{server_name}"]
+            csp["connect-src"] += [f"ws://{config.site.server_name}"]
 
     talisman.init_app(
         app, content_security_policy=csp, force_https=config.app.force_https

app/auth.py
@@ -17,7 +17,6 @@ from .models import (
     UserAuthSource,
     UserCrypto,
     UserStatus,
-    SiteMetadata,
     Sub,
     SubSubscriber,
 )
@@ -434,18 +433,6 @@ class AuthProvider:
 auth_provider = AuthProvider()
 
 
-def registration_is_enabled():
-    try:
-        enable_registration = SiteMetadata.get(
-            SiteMetadata.key == "enable_registration"
-        )
-        if enable_registration.value in ("False", "0"):
-            return False
-    except SiteMetadata.DoesNotExist:
-        pass
-    return True
-
-
 # Someday config.auth.require_valid_emails may move to site metadata.
 def email_validation_is_required():
     return config.auth.require_valid_emails
@@ -481,7 +468,7 @@ def create_user(username, password, email, invite_code, existing_user):
             verified_email=False,
             status=status,
         )
-    if misc.enableInviteCode():
+    if config.site.require_invite_code:
         UserMetadata.create(uid=user.uid, key="invitecode", value=invite_code)
 
     defaults = misc.getDefaultSubs()

app/forms/admin.py
@@ -3,6 +3,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField, FileField
 from wtforms import IntegerField, RadioField, FieldList, SelectField
+from wtforms import HiddenField
 from wtforms.validators import DataRequired, Length, Regexp
 from flask_babel import lazy_gettext as _l
 
@@ -112,3 +113,8 @@ class CreateInviteCodeForm(FlaskForm):
 
 class SetSubOfTheDayForm(FlaskForm):
     sub = StringField(_l("Sub"))
+
+
+class ChangeConfigSettingForm(FlaskForm):
+    setting = HiddenField()
+    value = StringField()

app/html/admin/configuration.html
@@ -1,0 +1,79 @@
+@extends("shared/layout.html")
+@require(config_data, form)
+@def title():
+Admin |\
+@end
+
+@def sidebar():
+@include('shared/sidebar/admin.html')
+@end
+
+@def main():
+<div id="container">
+  <div id="center-container">
+    <div class="content">
+      <h1>@{_('Site configuration')}</h1>
+      <p class="helper-text">@{_('See the <a href="%(url)s">Site log</a> for the history of changes to configuration values.', url=url_for('site.view_sitelog'))!!html}</p>
+      <div class="error hide"></div>
+      <div class="admin section users">
+        <div class="col-12">
+          <table class="admin-config pure-table">
+            <thead>
+              <tr>
+                <th class="admin-config-name">@{_('Setting Name')}</th>
+                <th class="admin-config-value">@{_('Current Value')}</th>
+                <th class="admin-config-action"></th>
+              </tr>
+            </thead>
+            <tbody>
+              @for data in config_data:
+              <tr>
+                <td><a class="admin-config-doc-toggle" id="@{data['name']}">▹</a>@{data['name']}</td>
+                <td id="@{data['name']}-value">
+                @if data['type'] == 'bool':
+                  @if data['value']:
+                    @{_('Enabled')}
+                  @else:
+                    @{_('Disabled')}
+                  @end
+                @else:
+                  @{data['value']}
+                @end
+                </td>
+                <td>
+                  @if data['type'] == 'bool':
+                  <a class="admin-config-edit" data-type="bool" data-setting="@{data['name']}">⇆</a>
+                  @else:
+                  <a class="icon admin-config-edit" data-type="@{data['type']}" data-setting="@{data['name']}" data-icon="edit"></a>
+                  @end
+                </td>
+              </tr>
+              <tr class="admin-config-doc hide" id="@{data['name']}-doc">
+                <td class="helper-text" colspan="3">@{data['doc']}</td>
+              </tr>
+              @end
+            </tbody>
+          </table>
+          <div id="form-container">
+            <div class="admin-config-edit-form hide">
+              <form method="POST" class="ajaxform pure-form" data-reload="true" action="@{url_for('do.admin_modify_config_setting')}">
+                @{form.csrf_token!!html}
+                <p>
+                  <span id="bool-label"></span>
+                  @{form.setting()!!html}
+                  @{form.value()!!html}
+                </p>
+                <p>
+                  <button type="submit" id="admin-config-edit-submit" name="change" class="pure-button" data-prog="@{_('Saving...')}"></button>
+                  <button type="button" id="admin-config-edit-cancel" class="pure-button">@{_("Cancel")}</button>
+                  <div class="alert div-error"></div>
+                </p>
+              </form>
+            </div>
+         </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@end

app/html/shared/sidebar/admin.html
@@ -13,4 +13,5 @@
 <a href="@{url_for('admin.domains', domain_type='link')}" class="sbm-post pure-button">@{_('Banned Domains')}</a>
 <a href="@{url_for('admin.domains', domain_type='email')}" class="sbm-post pure-button">@{_('Banned Email Domains')}</a>
 <hr>
+<a href="@{url_for('admin.configure')}" class="sbm-post pure-button">@{_('Site configuration')}</a>
 <a href="@{url_for('site.view_sitelog')}" class="sbm-post pure-button">@{_('Site log')}</a>

app/html/shared/sidebar/user.html
@@ -24,7 +24,7 @@
     @if request.path == url_for('user.edit_account'):
     <a href="@{url_for('user.delete_account')}" class="sbm-post pure-button @{(request.path == url_for('user.delete_account')) and 'pure-button-primary' or ''}">@{_('Delete account')}</a>
     @end
-    @if func.enableInviteCode():
+    @if config.site.require_invite_code:
         <hr/>
         <a href="@{url_for('user.invite_codes')}" class="sbm-post pure-button @{(request.path == url_for('user.invite_codes')) and 'pure-button-primary' or ''}">@{_('Invite codes')}</a>
     @end

commit 1e6a5ac81ed5e7fbe7a7edcd60c7551602077092

Fix incorrect reference to invitations config setting
Fix a regression introduced with #333 that results in a 500 on user
profile pages.

app/misc.py
@@ -597,7 +597,7 @@ def getInviteCodeInfo(uid):
 
     if not (
         current_user.is_admin()
-        or (config.invitations_visible_to_users and current_user.uid == uid)
+        or (config.site.invitations_visible_to_users and current_user.uid == uid)
     ):
         return info
 