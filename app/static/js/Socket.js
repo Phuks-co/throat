b0454caaa200b4cde52e87944b6a7168b656e319
globalistasis <funksoulxl@hotmail.com>
2021-12-05 00:07:43 +0100

    Replacing addZero with padStart

--- app/static/js/Socket.js
+++ app/static/js/Socket.js
@@ -353,14 +353,10 @@ socket.on('msg', function(data){
     data.user = "*";
     xc=xc + " msg-ac";
   }
-  function addZero(i) {
-    if (i < 10) { i = "0" + i }
-    return i;
-  }
   var d = new Date(data.time * 1000);
-  var hours = addZero(d.getHours());
-  var minutes = addZero(d.getMinutes());
-  var seconds = addZero(d.getSeconds());
+  var hours = String(d.getHours()).padStart(2, '0');
+  var minutes = String(d.getMinutes()).padStart(2, '0');
+  var seconds = String(d.getSeconds()).padStart(2, '0');
   cont.innerHTML = cont.innerHTML + '<div class="msg ' + xc + '"><span class="msgtime">(' + hours + ':' + minutes + ':' + seconds + ') </span><span class="msguser">' + data.user + '&gt;</span><span class="damsg">' + anchorme(ircStylize(data.msg.replace(/  /g, '&#32;&nbsp;')), { emails: false, files: false, attributes: [{ name: "target", value: "blank" }] }).replace(reg, "$1<a href='/u/$3'>$2$3</a>$4") + '</span></div>';
   var k = document.getElementsByClassName('msg')
   if(k.length > 3){
